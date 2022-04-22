7051b6e1e5ed72b9f12812659a1e347f0b2e2a3b
Matheus <mvbturatti@gmail.com>
2022-04-22 00:07:19 -0300

    solution from exercise rpn calculator

--- rpn-calculator/lib/rpn_calculator.ex
+++ rpn-calculator/lib/rpn_calculator.ex
@@ -1,13 +1,34 @@
 defmodule RPNCalculator do
+  @doc """
+  Simple exercise around rescuing concepts, capturing exception cases
+  and returning with appropriate message
+  """
+
+
+  @type verbose() :: {:ok, String.t()} | :error | {:error, String.t()}
+  @type calculate() :: {:ok, String.t()} | :error
+
+  @spec calculate!(stack :: [], operation :: (any -> String.t())) :: String.t()
   def calculate!(stack, operation) do
-    # Please implement the calculate!/2 function
+    operation.(stack)
   end
 
+  @spec calculate(stack :: [], operation :: (-> String.t())) :: calculate()
   def calculate(stack, operation) do
-    # Please implement the calculate/2 function
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      _ -> :error
+    end
   end
 
+  @spec calculate_verbose(stack :: [], operation :: (-> String.t())) :: verbose()
   def calculate_verbose(stack, operation) do
-    # Please implement the calculate_verbose/2 function
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      e in ArgumentError -> {:error, e.message}
+      _ -> :error
+    end
   end
 end
