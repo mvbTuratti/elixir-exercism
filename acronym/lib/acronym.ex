b8f929a4aa184145b63a89ae51221270ea63e747
Matheus <mvbturatti@gmail.com>
2022-05-07 11:54:54 -0300

    solution for exercise collatz conjucture and some modifications to acronym

--- acronym/lib/acronym.ex
+++ acronym/lib/acronym.ex
@@ -8,15 +8,15 @@ defmodule Acronym do
     pattern = :binary.compile_pattern([" ", "-", "_"])
     string
     |> String.split(pattern)
-    |> Stream.filter(&is_word?/1)
+    |> Stream.filter(&word?/1)
     |> Stream.filter(&String.trim/1)
     |> Stream.map(&String.at(&1, 0))
     |> Stream.map(&String.upcase/1)
     |> Enum.reduce(fn x, acc -> acc <> x end)
   end
 
-  @spec is_word?(String.t()) :: boolean()
-  defp is_word?(word) do
+  @spec word?(String.t()) :: boolean()
+  defp word?(word) do
     String.match?(word, ~r/\w/u)
   end
 end
---
+++ collatz-conjecture/lib/collatz_conjecture.ex
@@ -0,0 +1,30 @@
+defmodule CollatzConjecture do
+  @doc """
+  calc/1 takes an integer and returns the number of steps required to get the
+  number to 1 when following the rules:
+    - if number is odd, multiply with 3 and add 1
+    - if number is even, divide by 2
+  """
+
+  @spec calc(input :: pos_integer()) :: non_neg_integer()
+  def calc(input) when is_integer(input) and input >= 1 do
+    do_calc(input, 0)
+  end
+
+  defp do_calc(1, number), do: number
+  defp do_calc(input, number) do
+    case is_even?(input) do
+      true ->
+        input
+        |> div(2)
+        |> do_calc(number + 1)
+      false ->
+        input
+        |> Kernel.*(3)
+        |> Kernel.+(1)
+        |> do_calc(number + 1)
+    end
+  end
+
+  defp is_even?(num), do: rem(num, 2) == 0
+end
