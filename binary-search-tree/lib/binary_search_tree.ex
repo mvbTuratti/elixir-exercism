567563e9ba886b44e1e0c213196632f0fd7c7582
Matheus <mvbturatti@gmail.com>
2022-06-04 21:48:37 -0300

    start binary search tree

---
+++ binary-search-tree/lib/binary_search_tree.ex
@@ -0,0 +1,46 @@
+defmodule BinarySearchTree do
+  @type bst_node :: %{data: any, left: bst_node | nil, right: bst_node | nil}
+
+
+  @doc """
+  Create a new Binary Search Tree with root's value as the given 'data'
+  """
+  @spec new(any) :: bst_node
+  def new(data) do
+    %{data: data, left: nil, right: nil}
+  end
+
+  @doc """
+  Creates and inserts a node with its value as 'data' into the tree.
+  """
+  @spec insert(bst_node, any) :: bst_node
+  def insert(tree, data) do
+    case tree[:data] do
+      nil ->
+        new(data)
+      val when val <= data ->
+        %{tree | right: insert(tree[:right], data)}
+      val when val > data ->
+        %{tree | left: insert(tree[:left], data)}
+    end
+  end
+
+  @doc """
+  Traverses the Binary Search Tree in order and returns a list of each node's data.
+  """
+  @spec in_order(bst_node) :: [any]
+  def in_order(tree) do
+    do_in_order(tree)
+  end
+
+
+  defp do_in_order(tree) do
+    case tree[:left] do
+      nil ->
+        if tree[:right], do: [tree[:data] | do_in_order(tree[:right])] , else: [tree[:data]]
+      val ->
+        do_in_order(val)
+    end
+  end
+
+end
